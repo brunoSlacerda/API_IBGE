f21a1686a150c54ec5f004f343bbcb3f9d948849
Bruno Lacerda <brunosilvalacerda.bb@gmail.com>
2025-02-04 21:40:57 -0300

    Opção de escolher
    Trabalhando agora com as duas APIs disponíveis, onde o usuário tem a escolha de procurar um nome específico ou em ranking geral.

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import requests 
 import pandas as pd
 
-url = 'https://servicodados.ibge.gov.br/api/v2/censos/nomes/ranking'
 
-response = requests.request(method='GET',url=url)
+
+def defineUrl(escolha, nome = None):
+    if escolha == '1':
+        url = 'https://servicodados.ibge.gov.br/api/v2/censos/nomes/ranking'
+    elif escolha == '2':
+        url = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}'
+    return url
+
+escolha = ''
+while escolha != '1' and escolha != '2':
+    print('     - RANKING DE NOMES DO IBGE -     ')
+    print('ESCOLHA UMA OPÇÃO: ')
+    print('Opção 1 - Ver um ranking de nomes no geral')
+    print('Opção 2 - Escolher um nome específico por década')
+    escolha = input("Informe a opção desejada: ")
+    if escolha == '2':
+        nome = input("Informe o nome que deseja procurar: ")
+    else: 
+        nome = None
+
+response = requests.request(method='GET',url=defineUrl(escolha,nome))
 tabela = response.json()[0]['res']
-dt = pd.DataFrame(tabela)
-print(dt)
+df = pd.DataFrame(tabela)
+print(df)
