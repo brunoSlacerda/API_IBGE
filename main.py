20a89949e4a66a449da07197bf451c3300b47e51
Bruno Lacerda <brunosilvalacerda.bb@gmail.com>
2025-02-05 22:40:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,23 @@ def defineUrl(escolha, nome = None):
     elif escolha == '2':
         url = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}'
     return url
+def main():
+    escolha = ''
+    while escolha != '1' and escolha != '2':
+        print('     - RANKING DE NOMES DO IBGE -     ')
+        print('ESCOLHA UMA OPÇÃO: ')
+        print('Opção 1 - Ver um ranking de nomes no geral')
+        print('Opção 2 - Escolher um nome específico por década')
+        escolha = input("Informe a opção desejada: ")
+        if escolha == '2':
+            nome = input("Informe o nome que deseja procurar: ")
+        else: 
+            nome = None
 
-escolha = ''
-while escolha != '1' and escolha != '2':
-    print('     - RANKING DE NOMES DO IBGE -     ')
-    print('ESCOLHA UMA OPÇÃO: ')
-    print('Opção 1 - Ver um ranking de nomes no geral')
-    print('Opção 2 - Escolher um nome específico por década')
-    escolha = input("Informe a opção desejada: ")
-    if escolha == '2':
-        nome = input("Informe o nome que deseja procurar: ")
-    else: 
-        nome = None
+    response = requests.request(method='GET',url=defineUrl(escolha,nome))
+    tabela = response.json()[0]['res']
+    df = pd.DataFrame(tabela)
+    print(df)
 
-response = requests.request(method='GET',url=defineUrl(escolha,nome))
-tabela = response.json()[0]['res']
-df = pd.DataFrame(tabela)
-print(df)
+if __name__ == "__main__":
+    main()
