3a8b27964763f0d0b52ccba9a241ae2d5e288ab5
Bruno Lacerda <brunosilvalacerda.bb@gmail.com>
2025-02-11 19:53:09 -0300

    Uso da biblioteca rich para deixar melhor visualmente a resposta final.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests 
 import pandas as pd
-
+from rich.console import Console
+from rich.table import Table
 
 
 def defineUrl(escolha, nome = None, decada= None, localidade = None, sexo = None):
@@ -30,6 +31,19 @@ def defineUrl(escolha, nome = None, decada= None, localidade = None, sexo = None
     
     return url, params
 
+def display_data(df):
+    console = Console()
+    table = Table(title="\nResultados da Consulta")
+
+    for column in df.columns:
+        table.add_column(column)
+
+    for _, row in df.iterrows():
+        table.add_row(*[str(row[column]) for column in df.columns])
+
+    console.print(table)
+
+
 def main():
     escolha = ''
     while escolha != '1' and escolha != '2':
@@ -42,22 +56,26 @@ def main():
         if escolha == '2':
             nome = input("Informe o nome que deseja procurar: ")
             localidade = input("Informe uma localidade específica para procurar (OPCIONAL): ")
-            sexo = input("Informe um sexo para procurar (M ou F)(OPCIONAL): ") 
+            sexo = input("Informe um sexo para procurar (M ou F)(OPCIONAL): ").upper()
             decada = None
 
         elif escolha == '1':
             decada = input("Informe uma decada específica para procurar (OPCIONAL): ")
             localidade = input("Informe uma localidade específica para procurar (OPCIONAL): ")
-            sexo = input("Informe um sexo para procurar (M ou F)(OPCIONAL): ") 
+            sexo = input("Informe um sexo para procurar (M ou F)(OPCIONAL): ").upper() 
             nome = None
 
     url, params = defineUrl(escolha,nome,decada,localidade,sexo)
-    
+
+
+
+
     try:    
         response = requests.request(method='GET',url=url, params=params)
         tabela = response.json()[0]['res']
         df = pd.DataFrame(tabela)
-        print(df)
+        display_data(df)
+        
     except requests.exceptions.RequestException as e:
         print(f"Erro na requisição: {e}")
 
